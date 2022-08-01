17a6c6f2efb2cf4ffdcbef833a2c5ebd4b2459c4
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2022-08-01 17:17:57 +0900

    .

--- Kotlin_App_programming/Class13_Layout 객체/MyRecyclerview/app/src/main/java/org/techtown/recyclerview/Person.kt
+++
@@ -1,7 +0,0 @@
-package org.techtown.recyclerview
-
-data class Person(
-    val name:String?,
-    val mobile:String?
-    ) {   //data class로 만들면 JAVA처럼 private,get,set을 처리해주지 않아도 된다. 간단하게 속성만을 나열
-}
--- Kotlin_App_programming/Class13_Layout 객체/MyRecyclerview/app/src/main/java/org/techtown/recyclerview/PersonAdapter.kt
+++
@@ -1,4 +0,0 @@
-package org.techtown.recyclerview
-
-class PersonAdapter {
-}
