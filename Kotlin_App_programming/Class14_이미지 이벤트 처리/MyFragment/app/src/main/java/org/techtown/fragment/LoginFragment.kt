90406c0e7540802fa78b40c9e3f5b9e73c50a244
LEE-hyeon <84756586+LEE-hyeon0771@users.noreply.github.com>
2022-08-13 15:12:32 +0900

    fragment

--- Kotlin_App_programming/Class14_이미지 이벤트 처리/MyFragment/app/src/main/java/org/techtown/fragment/LoginFragment.kt
+++ Kotlin_App_programming/Class14_이미지 이벤트 처리/MyFragment/app/src/main/java/org/techtown/fragment/LoginFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_menu.view.*
 
 
 class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_login, container, false)
+        rootView.nextbutton.setOnClickListener {
+            val mainActivity = activity as MainActivity
+            mainActivity.onFragmentChanged(1)
+        }
+        return rootView
     }
 }
--- Kotlin_App_programming/Class14_이미지 이벤트 처리/MyFragment/app/src/main/java/org/techtown/fragment/MainActivity.kt
+++ Kotlin_App_programming/Class14_이미지 이벤트 처리/MyFragment/app/src/main/java/org/techtown/fragment/MainActivity.kt
@@ -1,11 +1,45 @@
 package org.techtown.fragment
 
+import android.icu.number.NumberFormatter.with
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var loginFragment: LoginFragment = LoginFragment()
+    var menuFragment: MenuFragment = MenuFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showLoginButton.setOnClickListener {
+            //supportFragmentManager.beginTransaction().replace(R.id.container, LoginFragment())
+                //.commit()
+            //commit() -> 앞의 함수들을 한번에 실행시킴
+            onFragmentChanged(0)
+
+        }
+        showMenuButton.setOnClickListener {
+            //supportFragmentManager.beginTransaction().replace(R.id.container, MenuFragment())
+                //.commit()
+            onFragmentChanged(1)
+        }
+    }
+
+    fun onFragmentChanged(index: Int){
+        when(index){
+            0 -> {
+                supportFragmentManager.beginTransaction().replace(R.id.container, LoginFragment())
+                    .commit()     //액티비티에 프래그먼트 추가(동작 가능)
+            }
+
+            1 -> {
+                supportFragmentManager.beginTransaction().replace(R.id.container, MenuFragment())
+                    .commit()     //액티비티에 프래그먼트 추가(동작 가능)
+            }
+        }
     }
 }
